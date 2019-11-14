26b13015f5e93d513503f826993c422271210ee4
Aleksandra Litvinova <aleksandra@MacBook-Pro-Aleksandra.local>
2019-11-14 12:58:04 +0700

    first commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,141 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import AppHeader from './components/app-header';
+import SearchPanel from './components/search-panel';
+import TodoList from './components/todo-list';
+import ItemStatusFilter from './components/item-status-filter';
+import ItemAddForm from './components/item-add-form';
+import './components/index.css';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+export default class App extends React.Component { 
+    
+      maxId = 100;
+    
+      state = {
+        items: [
+          { id: 1, label: 'Drink Coffee', important: false, done: false },
+          { id: 2, label: 'Learn React', important: true, done: false },
+          { id: 3, label: 'Make Awesome App', important: false, done: false }
+        ],
+        filter: 'all',
+        search: ''
+      };
+    
+      onItemAdded = (label) => {
+        this.setState((state) => {
+          const item = this.createItem(label);
+          return { items: [...state.items, item] };
+        })
+      };
+    
+      toggleProperty = (arr, id, propName) => {
+        const idx = arr.findIndex((item) => item.id === id);
+        const oldItem = arr[idx];
+        const value = !oldItem[propName];
+    
+        const item = { ...arr[idx], [propName]: value } ;
+        return [
+          ...arr.slice(0, idx),
+          item,
+          ...arr.slice(idx + 1)
+        ];
+      };
+    
+      onToggleDone = (id) => {
+        this.setState((state) => {
+          const items = this.toggleProperty(state.items, id, 'done');
+          return { items };
+        });
+      };
+    
+      onToggleImportant = (id) => {
+        this.setState((state) => {
+          const items = this.toggleProperty(state.items, id, 'important');
+          return { items };
+        });
+      };
+    
+      onDelete = (id) => {
+        this.setState((state) => {
+          const idx = state.items.findIndex((item) => item.id === id);
+          const items = [
+            ...state.items.slice(0, idx),
+            ...state.items.slice(idx + 1)
+          ];
+          return { items };
+        });
+      };
+    
+      onFilterChange = (filter) => {
+        this.setState({ filter });
+      };
+    
+      onSearchChange = (search) => {
+        this.setState({ search });
+      };
+    
+      createItem(label) {
+        return {
+          id: ++this.maxId,
+          label,
+          important: false,
+          done: false
+        };
+      }
+    
+      filterItems(items, filter) {
+        if (filter === 'all') {
+          return items;
+        } else if (filter === 'active') {
+          return items.filter((item) => (!item.done));
+        } else if (filter === 'done') {
+          return items.filter((item) => item.done);
+        }
+      }
+    
+      searchItems(items, search) {
+        if (search.length === 0) {
+          return items;
+        }
+    
+        return items.filter((item) => {
+          return item.label.toLowerCase().indexOf(search.toLowerCase()) > -1;
+        });
+      }
+    
+      render() {
+        const { items, filter, search } = this.state;
+        const doneCount = items.filter((item) => item.done).length;
+        const toDoCount = items.length - doneCount;
+        const visibleItems = this.searchItems(this.filterItems(items, filter), search);
+    
+        return (
+          <div className="todo-app">
+            <AppHeader toDo={toDoCount} done={doneCount}/>
+    
+            <div className="search-panel d-flex">
+              <SearchPanel
+                onSearchChange={this.onSearchChange}/>
+    
+              <ItemStatusFilter
+                filter={filter}
+                onFilterChange={this.onFilterChange} />
+            </div>
+    
+            <TodoList
+              items={ visibleItems }
+              onToggleImportant={this.onToggleImportant}
+              onToggleDone={this.onToggleDone}
+              onDelete={this.onDelete} />
+    
+            <ItemAddForm
+              onItemAdded={this.onItemAdded} />
+          </div>
+        );
+      };
+    };
+
+ReactDOM.render(<App />,
+     document.getElementById('root'));
